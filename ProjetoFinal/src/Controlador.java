db07c7608a4014c8e7ffb7a1736cf9e953dd356d
Diogo Porto <diogo.g.porto@gmail.com>
2024-05-23 00:09:24 +0100

    commitGrafico7

--- ProjetoFinal/src/Controlador.java
+++ ProjetoFinal/src/Controlador.java
@@ -4,6 +4,7 @@ import javax.swing.AbstractAction;
 import javax.swing.Timer;
 
 class Controlador {
+
     /*
     private Simulador sl;
     private final InterfaceGrafica view;
@@ -20,70 +21,41 @@ class Controlador {
 
 
     private void updateGameViewDisplay() {
-        int startx = 20;
-        int starty = 20;
-        double CellWidth = 20.0;
-        int numOfRows = this.model.getArenaHeight();
-        int numOfColumn = this.model.getArenaWidth();
-        Celula[][] Cells = new Celula[numOfRows][numOfColumn];
-        for(int i = 0; i < Cells.length; i++) {
-            double curretYPos = (double)i * CellWidth + (double)starty;
-
-            for(int j = 0; j < Cells[i].length; j++) {
-                double curentXPos = (double)j * CellWidth + (double)startx;
-                int currentCellType = this.model.getCellType(i, j);
-                Cells[i][j] = new Celula(currentCellType);
-                Cells[i][j].setRect(curentXPos, curretYPos, CellWidth, CellWidth);
-            }
-        }
-
-        this.view.setCells(Cells);
         this.view.repaint();
     }
 
     private void performOneStep() {
-        this.model.NextStep();
-        this.spanel.setScoreLabel(this.model.getCurrentScore());
-        if (!this.model.getPlayingMode()) {
-            this.stopTime();
-            this.npanel.updatePanelForPlayMode(false);
-        }
+        this.sl.getArena().atualizar();
+        this.painel.setScoreLabel(this.sl.getArena().getPontuacao());
     }
 
     private void initialpanelViewListeners() {
         this.painel.addPlayButtonListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
-                Controlador.this.sl.setPlayingMode(!Controlador.this.model.getPlayingMode());
-                JogoController.this.stopTime();
-                if (JogoController.this.model.getPlayingMode()) {
-                    JogoController.this.model.setArrowKey(JogoController.this.model.randomizeArrow());
-                    JogoController.this.startGame();
-                    JogoController.this.npanel.updatePanelForPlayMode(true);
-                } else {
-                    JogoController.this.npanel.updatePanelForPlayMode(false);
-                }
-
+                    Controlador.this.model.setArrowKey(Controlador.this.model.randomizeArrow());
+                    Controlador.this.startGame();
+                    Controlador.this.painel.updatePanelForPlayMode(true);
             }
         });
 
         this.view.addKeyArrowListener(38, "up", new AbstractAction() {
             public void actionPerformed(ActionEvent ae) {
-                JogoController.this.model.setArrowKey("up");
+                Controlador.this.model.setArrowKey("up");
             }
         });
         this.view.addKeyArrowListener(40, "down", new AbstractAction() {
             public void actionPerformed(ActionEvent ae) {
-                JogoController.this.model.setArrowKey("down");
+                Controlador.this.model.setArrowKey("down");
             }
         });
         this.view.addKeyArrowListener(39, "right", new AbstractAction() {
             public void actionPerformed(ActionEvent ae) {
-                JogoController.this.model.setArrowKey("right");
+                Controlador.this.model.setArrowKey("right");
             }
         });
         this.view.addKeyArrowListener(37, "left", new AbstractAction() {
             public void actionPerformed(ActionEvent ae) {
-                JogoController.this.model.setArrowKey("left");
+                Controlador.this.model.setArrowKey("left");
             }
         });
     }
@@ -98,7 +70,7 @@ class Controlador {
     private void startGame() {
         this.gameTimer = new Timer(100, new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
-                JogoController.this.startOneStepThread();
+                startOneStepThread();
             }
         });
         this.gameTimer.start();
@@ -107,13 +79,14 @@ class Controlador {
     private void startOneStepThread() {
         (new Thread(new Runnable() {
             public void run() {
-                synchronized(JogoController.this.model) {
-                    JogoController.this.performOneStep();
-                    JogoController.this.updateGameViewDisplay();
+                synchronized(Controlador.this.sl) {
+                    Controlador.this.performOneStep();
+                    Controlador.this.updateGameViewDisplay();
                 }
             }
         })).start();
     }
 
      */
+
 }
