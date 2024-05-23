6546ddbd5a87d0c41a3740351e3f698c36dfade8
Diogo Porto <diogo.g.porto@gmail.com>
2024-05-23 17:17:44 +0100

    commitGrafico9

--- ProjetoFinal/src/InterfaceGrafica.java
+++ ProjetoFinal/src/InterfaceGrafica.java
@@ -4,14 +4,66 @@ import javax.swing.Action;
 import javax.swing.JComponent;
 import javax.swing.KeyStroke;
 
-public class InterfaceGrafica extends JComponent {
+public class InterfaceGrafica extends JComponent{
     private Simulador sl;
     private Painel painel;
-    private View view;
-    private Controlador controlador;
+    private Ponto topLeftCorner;
+    private double dimensao;
+    public InterfaceGrafica(Simulador sl) {
+        this.sl = sl;
+    }
 
-    public InterfaceGrafica() {
 
+    public void addKeyArrowListener(int keyCode, String Name, Action action) {
+        this.getInputMap(2).put(KeyStroke.getKeyStroke(keyCode, 0), Name);
+        this.getActionMap().put(Name, action);
+    }
+
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2d = (Graphics2D)g;
+        Ponto p;
+
+        for(int i = 1; i < this.sl.getArena().getAltura(); i++) {
+            for(int j = 1; j < this.sl.getArena().getLargura(); j++) {
+                p = new Ponto(i, j);
+
+                g2d.setColor(Color.WHITE);
+                this.dimensao = 1;
+                this.topLeftCorner = this.sl.getArena().getVerticesFromCentroid(p).get(0);
+
+                //Cobra
+                for(int k = 0; k < this.sl.getArena().getCobra().getCobra().size(); k++) {
+                    if (p.equals(this.sl.getArena().getCobra().getCobra().get(k).findCentroide())) {
+                        g2d.setColor(Color.GREEN);
+                        break;
+                    }
+                }
+
+                //Comida
+                if (p.equals(this.sl.getArena().getComida().getPosicaoComida())) {
+                    if(this.sl.getTipoComida().equals("quadrado")){
+                        this.topLeftCorner = this.sl.getArena().getVerticesFromCentroid(this.sl.getArena().getComida().getPosicaoComida()).get(0);
+                    }
+                    else{
+                        this.topLeftCorner = p;
+                    }
+                    g2d.setColor(Color.RED);
+                }
+
+                //Obstaculos
+                for(int l = 0; l < this.sl.getArena().getObstaculos().size(); l++){
+                    if (p.equals(this.sl.getArena().getObstaculos().get(l).getPosicao())){
+                        this.topLeftCorner = this.sl.getArena().getObstaculos().get(l).getPoligono().getP().get(0);
+                        g2d.setColor(Color.BLUE);
+                        break;
+                    }
+                }
+
+                //g2d.draw(this.snakeGame[i][j].getRect());
+                g2d.fill(new Rectangle((int)this.topLeftCorner.getX(), (int)this.topLeftCorner.getY(), (int)this.dimensao, (int)this.dimensao));
+            }
+        }
+        //g2d.drawRect(20, 20, 400, 400);
     }
 
 
--- ProjetoFinal/src/Runner.java
+++ ProjetoFinal/src/Runner.java
@@ -5,6 +5,9 @@
  * @version 1.0
  */
 
+import javax.swing.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.Scanner;
 
 public class Runner {
@@ -21,7 +24,7 @@ public class Runner {
         this.sc = new Scanner(System.in);
         this.it = new InterfaceTextual(sl);
         this.cz = new CobraZarolha(sl);
-        //this.ig = new InterfaceGrafica(sl);
+        this.ig = new InterfaceGrafica(sl);
     }
 
 
@@ -87,10 +90,66 @@ public class Runner {
             }
         }
     }
+        public void runGrafica() {
+        /*
+            this.frame = new GameFrame();
+            this.painel = new Painel();
 
-    public void runGrafica(){
-        this.frame = new GameFrame();
-        this.painel = new Painel();
-    }
+         */
+            // Cria a janela principal para a interface gráfica
+            JFrame frame = new JFrame("Snake Game");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(400, 400); // Definir o tamanho com base na largura e altura da arena, possivelmente escalado
+            frame.setResizable(false);
 
+            // Adiciona a interface gráfica ao frame
+            frame.add(this.ig);
+            frame.pack();
+            frame.setVisible(true);
+
+            // Adiciona o KeyListener para capturar entrada do teclado
+            frame.addKeyListener(new KeyAdapter() {
+                @Override
+                public void keyPressed(KeyEvent e) {
+                    switch (e.getKeyCode()) {
+                        case KeyEvent.VK_LEFT:
+                            sl.proximoPasso("L");
+                            break;
+                        case KeyEvent.VK_RIGHT:
+                            sl.proximoPasso("R");
+                            break;
+                        case KeyEvent.VK_UP:
+                            sl.proximoPasso("U");
+                            break;
+                        case KeyEvent.VK_DOWN:
+                            sl.proximoPasso("D");
+                            break;
+                        case KeyEvent.VK_ESCAPE:
+                            System.exit(0);  // Encerra o jogo
+                            break;
+                    }
+                    frame.repaint();  // Solicita a re-renderização da janela após cada movimento
+                }
+            });
+
+            // Loop do jogo para modo automático
+            if (this.sl.getModoJogo().equals("automatico")) {
+                boolean isRunning = true;
+                while (isRunning) {
+                    try {
+                        Thread.sleep(300); // Intervalo entre os movimentos da cobra
+                    } catch (InterruptedException ex) {
+                        Thread.currentThread().interrupt();
+                    }
+
+                    String nextDirection = cz.nextDirection(); // Obtém a próxima direção da cobra zarolha
+                    if ("Q".equals(nextDirection)) {
+                        isRunning = false;
+                    } else {
+                        sl.proximoPasso(nextDirection);
+                    }
+                    frame.repaint();  // Re-renderiza após cada passo automático
+                }
+            }
+        }
 }
